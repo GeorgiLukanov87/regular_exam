de4e6415d9b112a3b004cfcdd5dd3fe3f0c3fde9
Georgi St. Lukanov <goshiii87@gmail.com>
2023-06-24 08:15:29 +0200

    views

--- regular_exam/my_web/urls.py
+++ regular_exam/my_web/urls.py
@@ -1,1 +1,36 @@
-urlpatterns = ()
+from django.urls import path, include
+
+from regular_exam.my_web.views import index, dashboard, create_fruit, details_fruit, edit_fruit, delete_fruit, \
+    create_profile, edit_profile, delete_profile, details_profile
+
+urlpatterns = (
+    path('', index, name='index'),
+    path('dashboard/', dashboard, name='dashboard'),
+
+    path('create/', create_fruit, name='create-fruit'),
+    path('<int:pk>/details/', details_fruit, name='details-fruit'),
+    path('<int:pk>/edit/', edit_fruit, name='edit-fruit'),
+    path('<int:pk>/delete/', delete_fruit, name='delete-fruit'),
+
+    path('profile/', include([
+        path('create/', create_profile, name='create-profile'),
+        path('edit/', edit_profile, name='edit-profile'),
+        path('delete/', delete_profile, name='delete-profile'),
+        path('details/', details_profile, name='details-profile'),
+    ]))
+)
+
+"""
+http://localhost:8000/ - index page
+http://localhost:8000/dashboard/ - dashboard page
+
+http://localhost:8000/create/ - fruit create page
+http://localhost:8000/1/details/ - fruit details page
+http://localhost:8000/1/edit/ - fruit edit page
+http://localhost:8000/1/delete/ - fruit delete page
+
+http://localhost:8000/profile/create/ - profile create page
+http://localhost:8000/profile/details/ - profile details page
+http://localhost:8000/profile/edit/ - profile edit page
+http://localhost:8000/profile/delete/ - profile delete page
+"""
--- regular_exam/my_web/views.py
+++ regular_exam/my_web/views.py
@@ -1,3 +1,43 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    return render(request, 'common/index.html')
+
+
+def dashboard(request):
+    return render(request, 'common/dashboard.html')
+
+
+# FRUIT VIEWS:
+def create_fruit(request):
+    return render(request, 'fruit/create-fruit.html')
+
+
+def details_fruit(request, pk):
+    return render(request, 'fruit/details-fruit.html')
+
+
+def edit_fruit(request, pk):
+    return render(request, 'fruit/edit-fruit.html')
+
+
+def delete_fruit(request, pk):
+    return render(request, 'fruit/delete-fruit.html')
+
+
+# PROFILE VIEWS:
+def create_profile(request):
+    return render(request, 'profile/create-profile.html')
+
+
+def edit_profile(request):
+    return render(request, 'profile/edit-profile.html')
+
+
+def delete_profile(request):
+    return render(request, 'profile/delete-profile.html')
+
+
+def details_profile(request):
+    return render(request, 'profile/details-profile.html')
